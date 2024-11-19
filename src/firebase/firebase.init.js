1df1ef0ee075dc98546d908fab20ad93c823f34e
abujaforhadi <abujaforhadi@gmail.com>
2024-11-19 11:07:33 +0600

    firebase env setup

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,16 +1,15 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBS-ZZo-p1qP2jrpg9Pe6hhEo_UQh5he8I",
-  authDomain: "careerbd-1301d.firebaseapp.com",
-  projectId: "careerbd-1301d",
-  storageBucket: "careerbd-1301d.firebasestorage.app",
-  messagingSenderId: "907695376001",
-  appId: "1:907695376001:web:3dc953cc5217082bb8ef80"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export default app;
